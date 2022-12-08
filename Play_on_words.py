9faf3d00d1ac8d0b64c250a41c0c2f2a876d9278
Tn-ko <99136238+Tn-ko@users.noreply.github.com>
2022-12-08 19:08:00 +0300

    Add files via upload

---
+++ Play_on_words.py
@@ -0,0 +1,77 @@
+import random
+
+my_file = (open('C:/Users/User/Desktop/слова.txt'))
+text = my_file.read()
+my_file.close()
+words = []
+available_words = list(words)
+for word in text.split():
+  words.append(word)
+  available_words = list(words)
+  random.shuffle(available_words)
+  anyword = available_words.pop()
+print("Рандомное слово " + anyword)
+
+def playing(anyword):
+  game_is_over = False
+  while not game_is_over:
+    mess = input()
+    if anyword[-1] == "ъ"  or anyword[-1] ==  "ь" or anyword[-1] == "ы":
+          if anyword[-2] == mess.lower()[0]:
+            print(anyword)
+            for candidant in available_words:
+                if candidant.lower()[0] == mess.lower()[-1]:
+                  anyword = candidant
+                  print(anyword)
+                  available_words.remove(candidant)
+                  break
+          else:
+            print("Неверно, слово должно начинаться с буквы " + anyword[-2])
+    elif mess.lower()[0] != anyword.lower()[-1]:
+        print("Неверно, слово должно начинаться с буквы " + anyword[-1])       
+    else:
+      for candidant in available_words:
+          if candidant.lower()[0] == mess.lower()[-1]:
+            anyword = candidant
+            print(anyword)
+            available_words.remove(candidant)
+            break
+          elif mess.lower()[-1] == "ъ" or mess.lower()[-1] == "ь" or mess.lower()[-1] == "ы":
+              if candidant[0].lower() == mess[-2].lower():
+                anyword = candidant
+                print(anyword)
+                available_words.remove(candidant)
+                break  
+      else:
+          print("Словарный запас был иссечён, схожу за новой порцией.")
+          game_is_over = True  
+
+        
+playing(anyword)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
+
+
+
+    
+
+              
+
+
